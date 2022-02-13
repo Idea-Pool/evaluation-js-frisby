7f7836494c5df7ce1777032c41f9e8d323f959a0
lmonika <laszlo.monika@logmein.com>
2022-02-13 12:27:44 +0100

    added test cases

--- tests/movie.test.js
+++ tests/movie.test.js
@@ -13,5 +13,23 @@ it('should return schema', function () {
       'id': Joi.number().integer().required(),
       'original_language': Joi.string().required(),
       'original_title': Joi.string().required()
-    });
+    })
+});
+
+it('should add a rating to a movie', async function () {
+  const response = await frisby.get('https://api.themoviedb.org/3/authentication/guest_session/new?api_key=e231ae412af0eaab685d11c26505bda6');
+  const session_id = response.json.guest_session_id;
+  console.log(session_id);
+  return frisby.setup({
+    request: {
+      haders: {
+        'Content-Type': 'application/json',
+        'charset': 'utf-8'
+      }
+    }
+  })
+  .post(`https://api.themoviedb.org/3/movie/818647/rating?api_key=e231ae412af0eaab685d11c26505bda6&guest_session_id=${session_id}`, {
+    "value": 8.0
+  })
+    .expect('status', 201);
 });
