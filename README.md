a60392657b35ad1c969051c5ef781d51f4782bfe
Monika_Laszlo <monika.laszlo@wolterskluwer.com>
2022-05-01 16:30:43 +0200

    updated Readme.md and package.json

--- README.md
+++ README.md
@@ -1,31 +1,20 @@
-> To use this template, clone a new repository based on this and
-> 1. Fill out this README.md, guided by the tasks below
-> 1. Implement the test cases in the choosen framework which are listed in [TESTCASES_UI.md](TESTCASES_UI.md) or [TESTCASES_API.md](TESTCASES_API.md), depending on the type of the tests.
-> 1. Delete this quotation
-> 1. Delete tasks in all files
+# evaluation-js-frisby
 
-# evaluation-js-template
-
-- [ ] Update name to `evaluation-js-(framework name)` here and in `package.json` too, including repository URLs.
-
-This is a template repository to create test-ware for evaluating certain (**JS**) test automation tools.
-
-- [ ] Update description to describe purpose of this repository, e.g. This is a test-ware for evaluation FRAMEWORK test automation tool.
+This is a template repository to create test-ware for evaluating Frisby JS test automation tool.
 
 ## Prerequisites
 
 1. Node.js LTS
-1. [ ] Add additional prerequisites if necessary
 
 ## Framework used
 
-- [ ] Add name of the framework
-- [ ] Add URL to the homepage and API docs of the framework
-- [ ] Add brief description of the framework
+- Frisby JS
+- https://docs.frisbyjs.com/
+- Frisby makes REST API testing easy, fast, and fun.
 
 ## Test cases
 
-The implemented test cases can be found in [TESTCASES_UI.md](TESTCASES_UI.md) or [TESTCASES_API.md](TESTCASES_API.md), depending on the type of the tests.
+The implemented test cases can be found in [TESTCASES_API.md](TESTCASES_API.md).
 
 ## Setup
 
@@ -33,12 +22,8 @@ The implemented test cases can be found in [TESTCASES_UI.md](TESTCASES_UI.md) or
 npm install
 ```
 
-- [ ] Add additional setup instructions if necessary
-
 ## Execution
 
 ```bash
-npm test
+jest
 ```
-
-- [ ] Add additional instructions to run tests
--- tests/movie.test.js
+++ tests/movie.test.js
@@ -1,81 +1,81 @@
-const frisby = require('frisby');
-const Joi = require('joi');
-const prepareUrl = (url, session_id = '') => {
-  return `${url}?api_key=e231ae412af0eaab685d11c26505bda6&guest_session_id=${session_id}`
-}
+const frisby = require("frisby");
+const Joi = require("joi");
+const prepareUrl = (url, session_id = "") => {
+    return `${url}?api_key=e231ae412af0eaab685d11c26505bda6&guest_session_id=${session_id}`;
+};
 
-describe('Movie test', () => {
-  let session_id;
-  beforeAll(async() => {
-    frisby.globalSetup({
-      request: {
-        headers: {
-          'Content-Type': 'application/json',
-          'charset': 'utf-8'
-        },
-        baseUrl: 'https://api.themoviedb.org/3'
-      },
+describe("Movie test", () => {
+    let session_id;
+    beforeAll(async() => {
+        frisby.globalSetup({
+            request: {
+                headers: {
+                    "Content-Type": "application/json",
+                    "charset": "utf-8"
+                },
+                baseUrl: "https://api.themoviedb.org/3"
+            },
     
-    });
-    const response = await frisby.get(prepareUrl('/authentication/guest_session/new'));
-    session_id = response.json.guest_session_id;
-  });
-
-  it('should return 200 status code', function () {
-    return frisby.get(prepareUrl('/movie/818647'))
-      .expect('status', 200);
-  });
-
-  it('should return schema', function () {
-    return frisby.get(prepareUrl('/movie/818647'))
-      .expect('json', 'original_title', 'A través de mi ventana')
-      .expect('jsonTypes', {
-        'id': Joi.number().integer().required(),
-        'original_language': Joi.string().required(),
-        'original_title': Joi.string().required()
-      })
-  });
-
-  it('should add a rating to a movie', async function () {
-    return frisby.post(prepareUrl(`/movie/818647/rating`, session_id), {
-        "value": 8.0
-      })
-      .expect('status', 201)
-      .expect('bodyContains', 'Success.');
-  });
-
-  it('should return proper status message', async function () {
-    const response = await frisby.post(prepareUrl(`/movie/818647/rating`, session_id), {
-        "value": 8.0
-      });
-
-    const status_message = response.json.status_message;
-    expect(status_message).toEqual('Success.');
-  });
-
-  it('should not add an invalid rating to a movie', async function () {
-    const response = await frisby.post(prepareUrl(`/movie/818647/rating`, session_id), {
-      "value": 10.1
+        });
+        const response = await frisby.get(prepareUrl("/authentication/guest_session/new"));
+        session_id = response.json.guest_session_id;
     });
 
-  const status_message = response.json.status_message;
-  expect(status_message).toEqual('Value too high: Value must be less than, or equal to 10.0.');
-  });
+    it("should return 200 status code", function () {
+        return frisby.get(prepareUrl("/movie/818647"))
+            .expect("status", 200);
+    });
 
-  it('should delete a movie rating', async function () {
-    return frisby.delete(prepareUrl(`/movie/818647/rating`, session_id))
-      .expect('status', 200);
-  });
+    it("should return schema", function () {
+        return frisby.get(prepareUrl("/movie/818647"))
+            .expect("json", "original_title", "A través de mi ventana")
+            .expect("jsonTypes", {
+                "id": Joi.number().integer().required(),
+                "original_language": Joi.string().required(),
+                "original_title": Joi.string().required()
+            });
+    });
 
-  it('should delete a movie rating', async function () {
-    const response = await frisby.delete(prepareUrl(`/movie/818647/rating`, session_id));
-    const status_message = response.json.status_message;
-    expect(status_message).toEqual('The item/record was deleted successfully.');
-  });
+    it("should add a rating to a movie", async function () {
+        return frisby.post(prepareUrl("/movie/818647/rating", session_id), {
+            "value": 8.0
+        })
+            .expect("status", 201)
+            .expect("bodyContains", "Success.");
+    });
 
-  it('should handle missing movie_id', function () {
-    return frisby.get(prepareUrl('/movie/'))
-      .expect('status', 404);
-  });
+    it("should return proper status message", async function () {
+        const response = await frisby.post(prepareUrl("/movie/818647/rating", session_id), {
+            "value": 8.0
+        });
+
+        const status_message = response.json.status_message;
+        expect(status_message).toEqual("Success.");
+    });
+
+    it("should not add an invalid rating to a movie", async function () {
+        const response = await frisby.post(prepareUrl("/movie/818647/rating", session_id), {
+            "value": 10.1
+        });
+
+        const status_message = response.json.status_message;
+        expect(status_message).toEqual("Value too high: Value must be less than, or equal to 10.0.");
+    });
+
+    it("should delete a movie rating", async function () {
+        return frisby.delete(prepareUrl("/movie/818647/rating", session_id))
+            .expect("status", 200);
+    });
+
+    it("should delete a movie rating", async function () {
+        const response = await frisby.delete(prepareUrl("/movie/818647/rating", session_id));
+        const status_message = response.json.status_message;
+        expect(status_message).toEqual("The item/record was deleted successfully.");
+    });
+
+    it("should handle missing movie_id", function () {
+        return frisby.get(prepareUrl("/movie/"))
+            .expect("status", 404);
+    });
 
 });
